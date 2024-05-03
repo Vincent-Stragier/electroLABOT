c159acc06476eeb101dc08a4c720b8170e9b04bd
ProdYJ <yannjacquet.guacharo@gmail.com>
2024-05-03 10:00:44 +0200

    Bug fixes
    - In motors.cpp : change ESP_LOGE to ESP_LOGI and add according TAG variable
    - In platformio.ini : set monitor speed to 9600 to match the value in the main code
    - In platformio.ini : set debug level to 0. With level 5, too many debug messages were sent, creating a lot of Serial messages, impacting the timing of the robot (accelleration too slow)
    - In platformio.ini : commenting useless dependencies
    - In electrolabbot_config.h : changing the acceleration value

--- pio_src/lib/electroLABBOT/src/motors.cpp
+++ pio_src/lib/electroLABBOT/src/motors.cpp
@@ -1,6 +1,8 @@
 /* Copyright 2022 electroLABBOT. All rights reserved. */
 #include "electroLABBOT.h"
 
+#define TAG "motors.cpp"
+
 // Queues used to handle motor speed and acceleration
 // QueueHandle_t xQueue_left_motor_speed;
 // QueueHandle_t xQueue_right_motor_speed;
@@ -125,41 +127,41 @@ void Motors::begin() {
   xQueue_left_motor_acceleration = xQueueCreate(1, sizeof(uint16_t));
 
   if (xQueue_left_motor_acceleration == NULL) {
-    ESP_LOGE("Error creating the queue \"xQueue_left_motor_acceleration\"");
+    ESP_LOGI(TAG, "Error creating the queue \"xQueue_left_motor_acceleration\"");
   }
 
   if (!xQueueSend(xQueue_left_motor_acceleration, &value, portMAX_DELAY)) {
-    ESP_LOGE("Error setting the queue \"xQueue_left_motor_acceleration\"");
+    ESP_LOGI(TAG, "Error setting the queue \"xQueue_left_motor_acceleration\"");
   }
 
   xQueue_right_motor_acceleration = xQueueCreate(1, sizeof(uint16_t));
 
   if (xQueue_right_motor_acceleration == NULL) {
-    ESP_LOGE("Error creating the queue \"xQueue_right_motor_acceleration\"");
+    ESP_LOGI(TAG, "Error creating the queue \"xQueue_right_motor_acceleration\"");
   }
 
   if (!xQueueSend(xQueue_right_motor_acceleration, &value, portMAX_DELAY)) {
-    ESP_LOGE("Error setting the queue \"xQueue_right_motor_acceleration\"");
+    ESP_LOGI(TAG, "Error setting the queue \"xQueue_right_motor_acceleration\"");
   }
 
   xQueue_right_motor_target_speed = xQueueCreate(1, sizeof(int16_t));
 
   if (xQueue_right_motor_target_speed == NULL) {
-    ESP_LOGE("Error creating the queue \"xQueue_right_motor_target_speed\"");
+    ESP_LOGI(TAG, "Error creating the queue \"xQueue_right_motor_target_speed\"");
   }
 
   if (!xQueueSend(xQueue_right_motor_target_speed, &value, portMAX_DELAY)) {
-    ESP_LOGE("Error setting the queue \"xQueue_right_motor_target_speed\"");
+    ESP_LOGI(TAG, "Error setting the queue \"xQueue_right_motor_target_speed\"");
   }
 
   xQueue_left_motor_target_speed = xQueueCreate(1, sizeof(int16_t));
 
   if (xQueue_left_motor_target_speed == NULL) {
-    ESP_LOGE("Error creating the queue \"xQueue_left_motor_target_speed\"");
+    ESP_LOGI(TAG, "Error creating the queue \"xQueue_left_motor_target_speed\"");
   }
 
   if (!xQueueSend(xQueue_left_motor_target_speed, &value, portMAX_DELAY)) {
-    ESP_LOGE("Error setting the queue \"xQueue_left_motor_target_speed\"");
+    ESP_LOGI(TAG, "Error setting the queue \"xQueue_left_motor_target_speed\"");
   }
 
   xTaskCreatePinnedToCore(motors_task, "motors_task", 10000, NULL, 1, NULL, 1);
